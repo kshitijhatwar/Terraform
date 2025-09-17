d150d7e574650d0a99f772562d8e51539c4f4bbf
Kshitij Hatwar <99862816+kshitijhatwar@users.noreply.github.com>
2025-09-17 17:00:10 +0530

    Create vmware.tf

---
+++ project3/vmware.tf
@@ -0,0 +1,22 @@
+terraform {
+  required_providers {
+    vsphere = {
+      source  = "hashicorp/vsphere"
+      version = ">= 2.0"
+    }
+  }
+}
+
+provider "vsphere" {
+  user                 = "khatwar@yotta.com"
+  password             = "Sushilamummy2@"
+  vsphere_server       = "10.0.24.20"
+  allow_unverified_ssl = true
+}
+data "vsphere_datacenter" "dc" {
+  name = "DatacenterName"
+}
+
+output "datacenter_id" {
+  value = data.vsphere_datacenter.dc.id
+}
