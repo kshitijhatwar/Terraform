bebded79e70bc57fe7e15bb8ac50bbadd1c192b3
kshitijhatwar <kshhat6685@gmail.com>
2025-09-17 22:48:03 +0530

    terraaform

--- project3/vmware.tf
+++ project3/vmware.tf
@@ -59,4 +59,44 @@ output "template_id" {
   value = data.vsphere_virtual_machine.template.id
 }
 
+resource "vsphere_virtual_machine" "vm" {
+  name             = "terraform-test-vm"
+  resource_pool_id = data.vsphere_compute_cluster.cluster.resource_pool_id
+  datastore_id     = data.vsphere_datastore.datastore.id
 
+  num_cpus = 2
+  memory   = 2048
+  guest_id = data.vsphere_virtual_machine.template.guest_id
+
+  scsi_type = data.vsphere_virtual_machine.template.scsi_type
+
+  network_interface {
+    network_id   = data.vsphere_network.network.id
+    adapter_type = data.vsphere_virtual_machine.template.network_interface_types[0]
+  }
+
+  disk {
+    label            = "disk0"
+    size             = data.vsphere_virtual_machine.template.disks.0.size
+    eagerly_scrub    = false
+    thin_provisioned = data.vsphere_virtual_machine.template.disks.0.thin_provisioned
+  }
+
+  clone {
+    template_uuid = data.vsphere_virtual_machine.template.id
+
+    customize {
+      linux_options {
+        host_name = "terraform-test"
+        domain    = "local"
+      }
+
+      network_interface {
+        ipv4_address = "10.0.3.167"
+        ipv4_netmask = 23
+      }
+
+      ipv4_gateway = "10.0.2.1"
+    }
+  }
+}
